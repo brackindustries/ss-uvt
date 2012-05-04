a7bb25258a4f944a11fc93b91615f030acc5e0e3
frank bossert <dev@mulonation.com>
2012-05-04 10:39:57 +0200

    updated github project url

--- ssuvt.php
+++ ssuvt.php
@@ -12,7 +12,7 @@ $uvt_min = JSMin::minify(file_get_contents('ss-uvt.js'));
 <p>This page helps in development of the SilverStripe Urlvartool. See <a href="http://doc.silverstripe.org/sapphire/en/reference/urlvariabletools">http://doc.silverstripe.org/sapphire/en/reference/urlvariabletools</a> for more information.</p>
 <p>You will need a PHP-enabled webserver to use it.</p>
 <p>It provides a bookmarklet. Drag this link to your browser's bookmark bar: <a href="javascript:<?php echo ($uvt_min); ?>">SS-UVT</a>, click the link to view it in action.</p>
-<p>This project is available at Github: <a href="https://"></a></p>
+<p>This project is available at Github: <a href="https://github.com/brackindustries/ss-uvt">https://github.com/brackindustries/ss-uvt</a></p>
 <script type="text/javascript" defer="defer">
 </script>
 <p></p>
